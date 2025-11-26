25d13d4cf15b75d420394e321090ccda1355aea6
Karnapuumamaheswari <karnapuumamaheswari022005@gmail.com>
2025-11-26 21:32:10 +0530

    Added js files

---
+++ question19/program.js
@@ -0,0 +1,14 @@
+var age = 20;
+
+function displayAge() {
+    console.log("Age inside displayAge():", age);
+}
+
+function changeAge() {
+    age = 25;
+    console.log("Age inside changeAge():", age);
+}
+
+displayAge();
+changeAge();
+displayAge();
